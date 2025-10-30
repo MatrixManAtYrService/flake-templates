ee8f33514a06deb69636b2ebb743cfa8d5581615
Matt Rixman <matt.rixman@clover.com>
2025-10-30 09:09:40 -0600

    added unison template

--- flake.nix
+++ flake.nix
@@ -6,13 +6,21 @@
       url = "github:MatrixManAtYrService/hello-template";
       flake = false;
     };
+    unison-template = {
+      url = "github:MatrixManAtYrService/unison-template";
+      flake = false;
+    };
   };
 
-  outputs = { self, hello-template, ... }: {
+  outputs = { self, hello-template, unison-template, ... }: {
     templates = {
       pycli = {
         path = hello-template;
         description = "A python CLI app";
+      };
+      unison-template = {
+        path = unison-template;
+        description = "A devshell with ucm";
       };
 
       defaultTemplate = self.templates.pycli;
